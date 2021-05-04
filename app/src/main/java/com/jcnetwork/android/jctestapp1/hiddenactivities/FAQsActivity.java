7e896286d2496acfed0dd89e0a6ce1ea082b7a2b
layla <layla.rohkohl@jcnetwork.de>
2021-05-04 10:09:36 +0200

    FAQ section filled up

--- app/src/main/java/com/jcnetwork/android/jctestapp1/hiddenactivities/FAQsActivity.java
+++ app/src/main/java/com/jcnetwork/android/jctestapp1/hiddenactivities/FAQsActivity.java
@@ -47,15 +47,51 @@ public class FAQsActivity extends AppCompatActivity {
         mList.setLayoutManager(manager);
 
 
-        // Create data
-        QandA qandA = new QandA("Question 1", "Answer 1");
-        qandAList.add(qandA);
-        QandA qand2 = new QandA("Was ist das JCNetwork Certified Junior Consultant Program?", "Das JCNetwork Certified Junior Consultant Program zeichnet herausragende Junior Consultants aus, die ein breites theoretisches Wissen sowie die praktische Anwendung theoretischer Kenntnisse vorweisen können. Die persönliche Exzellenz der Junior Consultants spiegelt direkt die Qualität der Beratungsleistungen wider, die durch die regionalen studentischen Unternehmensberatungen angeboten werden. Durch umfangreiche Maßnahmen zur Qualitätssicherung und der Unterstützung durch unseren Zertifizierungspartner UMS Consulting GmbH & Co. KG können wir garantieren, dass zertifizierte Junior Consultants über vielfältige beratungsrelevante Kompetenzen verfügen. Die Inhalte des Zertifikates resultieren aus den Anforderungen mit denen studentische Berater im Rahmen ihrer Tätigkeiten konfrontiert werden.");
+        // General
+        //        QandA qandA = new QandA("Question 1", "Answer 1"); TEMPLATE
+        //        qandAList.add(qandA);
+        QandA qand1 = new QandA("Was ist das JCNetwork Certified Junior Consultant Program?", "Das JCNetwork Certified Junior Consultant Program zeichnet herausragende Junior Consultants aus, die ein breites theoretisches Wissen sowie die praktische Anwendung theoretischer Kenntnisse vorweisen können. Die persönliche Exzellenz der Junior Consultants spiegelt direkt die Qualität der Beratungsleistungen wider, die durch die regionalen studentischen Unternehmensberatungen angeboten werden. Durch umfangreiche Maßnahmen zur Qualitätssicherung und der Unterstützung durch unseren Zertifizierungspartner UMS Consulting GmbH & Co. KG können wir garantieren, dass zertifizierte Junior Consultants über vielfältige beratungsrelevante Kompetenzen verfügen. Die Inhalte des Zertifikates resultieren aus den Anforderungen mit denen studentische Berater im Rahmen ihrer Tätigkeiten konfrontiert werden.");
+        qandAList.add(qand1);
+        QandA qand2 = new QandA("Wer ist unser Zertifizierungspartner?", "Die UMS Consulting GmbH & Co. KG ist die führende Strategieumsetzungsberatung und unterstützt Unternehmen weltweit dabei, vorhandene Potenziale aufzuzeigen und zu realisieren.");
         qandAList.add(qand2);
-        QandA qand3 = new QandA("Wer ist unser Zertifizierungspartner?", "Die UMS Consulting GmbH & Co. KG ist die führende Strategieumsetzungsberatung und unterstützt Unternehmen weltweit dabei, vorhandene Potenziale aufzuzeigen und zu realisieren.");
+        QandA qand3 = new QandA("Wie wird man JCNetwork Certified Junior Consultant?", "Alle Mitglieder einer dem JCNetwork angeschlossenen studentischen Unternehmensberatung können nach Erreichen vorgeschriebener Qualifikationen als Junior Consultant zertifiziert werden. Hierzu muss sowohl theoretisches Wissen erlangt werden, relevante Praxiserfahrung gesammelt werden und im Rahmen einer Abschlussprüfung ein reales Fallbeispiel aus dem Beratungsalltag absolvieren.");
         qandAList.add(qand3);
-        QandA qand4 = new QandA("Wie wird man JCNetwork Certified Junior Consultant?", "Alle Mitglieder einer dem JCNetwork angeschlossenen studentischen Unternehmensberatung können nach Erreichen vorgeschriebener Qualifikationen als Junior Consultant zertifiziert werden. Hierzu muss sowohl theoretisches Wissen erlangt werden, relevante Praxiserfahrung gesammelt werden und im Rahmen einer Abschlussprüfung ein reales Fallbeispiel aus dem Beratungsalltag absolvieren.");
+        QandA qand4 = new QandA("Welche Programmpunkte sind verpflichtend für den Teilnehmer?", "Das ist von Veranstaltung zu Veranstaltung unterschiedlich. Generell jedoch alle Workshops, bei denen du dich angemeldet hast. Plenen, außerhalb der Vorstandsrunde, sind Pflicht für mind. einen Vorstand aus jedem Verein. Außerdem sind die Firmenkontaktmesse und die MV verpflichtend.");
         qandAList.add(qand4);
+        // Workshops
+        QandA qand5 = new QandA("Wie bewerbe ich mich auf externe Workshops?", "Mit einem ausgefüllten Lebenslauf kannst du dich bei Anmeldestart auf externe Workshops bewerben. Pro Tipp: auf gleich mehrere bewerben, um größere Chancen zu haben, einen der begehrten Plätze zu ergattern.");
+        qandAList.add(qand5);
+        QandA qand6 = new QandA("Ich habe mich für mehrere externe Workshops in einem Slot beworben. Falls ich von mehreren Unternehmen zugelassen werde, kann ich mir den Workshop dann aussuchen?", "Nein, der Workshop wird vom JCNetwork zugeteilt.");
+        qandAList.add(qand6);
+        QandA qand7 = new QandA("Ich kann mich nicht auf externe Workshops bewerben. Woran kann das liegen?", "Entweder ist der Lebenslauf noch nicht ausgefüllt oder die Bewerbungsphase ist bereits abgelaufen.");
+        qandAList.add(qand7);
+        QandA qand8 = new QandA("Wie erhalte ich Credit-Points für meine Teilnahme an Schulungen?", "Unbedingt die Umfrage ausfüllen am Ende der Schulung. Um den Rest kümmert sich der JCNetwork Vorstand. Konkrete Fragen können direkt an den Vorstand für Weiterbildung gestellt werden.");
+        qandAList.add(qand8);
+        QandA qand9 = new QandA("Ich bin Referent und habe eine Frage bzgl. meines Workshops.", "Fragen direkt an Vorstand Weiterbildung.");
+        qandAList.add(qand9);
+        // Anmelden und Abmelden
+        QandA qand19 = new QandA("Ich war noch nie bei einer Veranstaltung. Wie kann ich mich anmelden?", "Falls du noch nicht registriert bist, registriere dich bitte zuerst unter https://intern.jcnetwork.de/register. Danach kannst du dich auf der Eventseite anmelden mit dem erstellten Konto.");
+        qandAList.add(qand19);
+        QandA qand10 = new QandA("Welche Unterschiede gibt es in den Anmeldeoptionen?", "Das unterscheidet sich von Veranstaltung zu Veranstaltung. Generell ist es abhängig von deiner Rolle in der Veranstaltung, z.B. normaler Junior Consultant oder Mithelfer bei der Ausrichtung des Events.");
+        qandAList.add(qand10);
+        QandA qand11 = new QandA("Alle Anmeldeoptionen sind ausgebucht. Wird die Anzahl an Plätzen nochmal aufgestockt?", "Falls die Anmeldeoptionen ausgebucht sind, wird alles dafür getan, weitere Kapazitäten zu organisieren und für die Anmeldung freizuschalten. Falls es zu einer Aufstockung der Plätze kommt, werden die Vereine über Social Media und E-Mail darauf hingewiesen. Die Vergabe der Plätze erfolgt nach dem First-Come-First-Serve Prinzip. Daher hopp-hopp in der Anmeldung!");
+        qandAList.add(qand11);
+        QandA qand12 = new QandA("Ich bin Referent für einen Workshop. Muss ich mich über die Eventseite anmelden?", "Ja.");
+        qandAList.add(qand12);
+        QandA qand13 = new QandA("Ich muss mich von den JCNetwork Days abmelden. Bei wem muss ich mich melden?", "Beim Vorstand Event Management.");
+        qandAList.add(qand13);
+        // Unterkunft
+        QandA qand14 = new QandA("Kann ich meine Unterkunft bzw. Zimmer wechseln?", "Nein. Nach Abschluss der Zuteilung werden keine Änderungen seitens des JCNetwork vorgenommen.");
+        qandAList.add(qand14);
+        QandA qand15 = new QandA("Ich möchte mit einer bestimmten Person auf ein Zimmer. An wen wende ich mich?", "Bitte schreibe deine Wunschperson in das Feld „Sonstiges“ im Anmeldeformular. Wir versuchen deinen Wunsch zu berücksichtigen, können jedoch nichts versprechen.");
+        qandAList.add(qand15);
+        // Finanzen und Bezahlung
+        QandA qand16 = new QandA("Ich benötige eine Rechnung bzw. Zahlungsbestätigung seitens des JCNetwork. Wie bekomme ich diese?", "Mit einer kurzen Anfrage beim Vorstand Finanzen und Recht.");
+        qandAList.add(qand16);
+        QandA qand17 = new QandA("Ich bin Referent, muss ich zahlen?", "Bei physischen Veranstaltungen immer, bei digitalen nur, wenn auch an anderen Veranstaltungspunkten teilgenommen wird.");
+        qandAList.add(qand17);
+        QandA qand18 = new QandA("Bekomme ich meine Teilnahmegebühr erstattet, wenn ich mich aus privaten Gründen abmelden muss?", "Normalerweise nicht. Besondere Umstände können mit Finanzen und Recht besprochen werden.");
+        qandAList.add(qand18);
 
 
         // Set up adapter
